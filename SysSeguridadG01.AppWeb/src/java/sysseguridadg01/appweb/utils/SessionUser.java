18411f844aaad0bbd2325fa0dac0e7523a1d311d
victor1206 <victor_rivas89@hotmail.com>
2023-07-31 11:14:16 -0600

    Se suben nuevos cambios

--- SysSeguridadG01.AppWeb/src/java/sysseguridadg01/appweb/utils/SessionUser.java
+++ SysSeguridadG01.AppWeb/src/java/sysseguridadg01/appweb/utils/SessionUser.java
@@ -29,4 +29,57 @@ public class SessionUser {
         session.setAttribute("user", pUsuario.getLogin());
         session.setAttribute("rol", pUsuario.getRol().getNombre());
     }
+    
+    public static boolean isAuth(HttpServletRequest request)
+    {
+        HttpSession session = (HttpSession) request.getSession();
+        boolean auth = session.getAttribute("auth") != null ? 
+                (boolean) session.getAttribute("auth"): false;
+        return auth;
+    }
+    
+    public static String getUser(HttpServletRequest request)
+    {
+        HttpSession session = (HttpSession) request.getSession();
+        String user = "";
+        if(SessionUser.isAuth(request))
+        {
+            user = session.getAttribute("user") != null ? 
+                    (String) session.getAttribute("user") : "";
+        }
+        return user;
+    }
+    
+    public static String getRol(HttpServletRequest request)
+    {
+        HttpSession session = (HttpSession) request.getSession();
+        String rol = "";
+        if(SessionUser.isAuth(request))
+        {
+            rol = session.getAttribute("rol") != null ? 
+                    (String) session.getAttribute("rol") : "";
+        }
+        return rol;
+    }
+    
+    public static void authorize(HttpServletRequest request, 
+                                 HttpServletResponse response,
+                                 IAuthorize pIAuthorize) throws ServletException,
+                                                                IOException
+    {
+        if(SessionUser.isAuth(request))
+        {
+          pIAuthorize.authorize();
+        }
+        else
+        {
+            response.sendRedirect("Usuario?accion=login");
+        }
+    }
+    
+    public static void cerrarSession(HttpServletRequest request)
+    {
+        HttpSession session = (HttpSession) request.getSession();
+        session.invalidate();
+    }
 }
--- SysSeguridadG01.AppWeb/src/java/sysseguridadg01/appweb/utils/Utilidad.java
+++ SysSeguridadG01.AppWeb/src/java/sysseguridadg01/appweb/utils/Utilidad.java
@@ -8,6 +8,39 @@ package sysseguridadg01.appweb.utils;
  *
  * @author victo
  */
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
 public class Utilidad {
+    public static String getParameter(HttpServletRequest request, String pKey, 
+                                     String pDefault)
+    {
+        String result = "";
+        String value = request.getParameter(pKey);
+        if(value != null && value.trim().length() > 0)
+        {
+            result = value;
+        }
+        else
+        {
+            result = pDefault;
+        }
+        return result;
+    }
     
+    public static void enviarError(String pError, HttpServletRequest request,
+                                   HttpServletResponse response) throws 
+                                   ServletException, IOException
+    {
+        request.setAttribute("error", pError);
+        request.getRequestDispatcher("Views/Shared/error.jsp").
+                forward(request, response);
+    }
+    
+    public static String obtenerRuta(HttpServletRequest request, String pStrRuta)
+    {
+        return request.getContextPath()+ pStrRuta;
+    }
 }
